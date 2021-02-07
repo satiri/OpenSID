54a7beed944b7786e234e3c8a7aa305064980dc1
satiri <satiri.a@gmail.com>
2021-02-07 22:10:39 +0700

    memisah script web dan admin

--- donjo-app/views/global/validasi_form.php
+++ donjo-app/views/global/validasi_form.php
@@ -1,4 +1,11 @@
 <script src="<?= base_url('assets/js/jquery.validate.min.js'); ?>"></script>
 <script src="<?= base_url('assets/js/validasi.js'); ?>"></script>
 <script src="<?= base_url('assets/js/localization/messages_id.js'); ?>"></script>
+
+<?php IF($_SESSION['UI'] === 'ADMIN'): ?>
 <script src="<?= base_url('assets/js/script.js'); ?>"></script>
+<?php ENDIF ?>
+
+<?php IF($_SESSION['UI'] === 'WEB'): ?>
+<script src="<?= base_url('assets/js/script-web.js'); ?>"></script>
+<?php ENDIF ?>
